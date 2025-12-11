4f021e763c9adc50b7575d9951c0448bdd6187b9
anhnq2410071-wq <anhnq2410071.usth.edu.vn>
2025-12-11 18:14:48 +0700

    lab4 done

---
+++ lab4/input.py
@@ -0,0 +1,44 @@
+import math
+import curses
+
+
+def get_param(stdscr, prompt):
+    stdscr.clear()
+    stdscr.addstr(0, 0, prompt)
+    stdscr.refresh()
+
+    curses.echo()
+    input_str = stdscr.getstr(1, 0, 20).decode(encoding="utf-8")
+    curses.noecho()
+    return input_str
+
+
+def input_mark(stdscr, student_name):
+    while True:
+        try:
+            val_str = get_param(stdscr, f"Input point {student_name} (0-20): ")
+            val = float(val_str)
+            if 0 <= val <= 20:
+                return math.floor(val)
+            else:
+                stdscr.addstr(2, 0, "point must between 0-20. Try again.")
+                stdscr.getch()
+        except ValueError:
+            stdscr.addstr(2, 0, "Error : Must be a number. Try again.")
+            stdscr.getch()
+
+
+def select_course(stdscr, course_manager):
+    while True:
+        try:
+            cid_str = get_param(stdscr, "Input the Id to input the point: ")
+            cid = int(cid_str)
+            course = course_manager.findCourseById(cid)
+            if course:
+                return course
+            else:
+                stdscr.addstr(2, 0, "Not found ID, Try again.")
+                stdscr.getch()
+        except ValueError:
+            stdscr.addstr(2, 0, "ID must be a number, Try again.")
+            stdscr.getch()
---
+++ lab4/output.py
@@ -0,0 +1,33 @@
+import curses
+
+
+def print_courses(stdscr, courses):
+    stdscr.clear()
+    stdscr.addstr(0, 0, "List courses", curses.A_BOLD)
+    row = 1
+    for c in courses:
+        stdscr.addstr(row, 0, f"ID: {c.getId()} | Name: {c.getName()}")
+        row += 1
+    stdscr.addstr(row + 1, 0, "continue")
+    stdscr.refresh()
+    stdscr.getch()
+
+
+def print_marks(stdscr, course):
+    stdscr.clear()
+    if not course:
+        return
+
+    stdscr.addstr(0, 0, f"List score courses: {course.getName()}", curses.A_BOLD)
+    row = 1
+    marks = course.getMarks()
+    students = course.getListStudents()
+
+    for s in students:
+        mark = marks.get(s.getId(), "not enter yet")
+        stdscr.addstr(row, 0, f"Student: {s.getName()} | score: {mark}")
+        row += 1
+
+    stdscr.addstr(row + 1, 0, "exit")
+    stdscr.refresh()
+    stdscr.getch()
